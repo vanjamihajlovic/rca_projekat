1662b8166fb8cb4d313d07f9841cb932c66dcff3
oli167 <116897781+oli167@users.noreply.github.com>
2024-04-29 14:59:50 +0200

    Jwt Token

---
+++ RedditClone/Helpers/JWT/JwtKey.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Helpers.JWT
+{
+    public class JwtKey
+    {
+        private readonly string _secretKey;
+
+        public JwtKey(string secretKey)
+        {
+            _secretKey = secretKey;
+        }
+
+        public SymmetricSecurityKey GetKey()
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        }
+    }
+}
---
+++ RedditClone/Helpers/JWT/JwtToken.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+//Klasa koja je odgovorna za generisanje JWT tokena koji se može koristiti za autentifikaciju i autorizaciju korisnika u aplikaciji
+//Claims su informacije o korisniku ili entitetu koji će biti enkodirane u tokenu, kao što je korisničko ime i uloge
+
+namespace Helpers.JWT
+{
+    public class JwtToken
+    {
+        private readonly JwtKey _key;
+        private readonly string _issuer; //Izdavač tokena, tj. entitet koji je generisao token
+        private readonly string _audience; //Auditorijum tokena, tj. entitet koji je namenjen da ga koristi
+
+        public JwtToken(string secretKey, string issuer, string audience)
+        {
+            _key = new JwtKey(secretKey);
+            _issuer = issuer;
+            _audience = audience;
+        }
+
+        public string GenerateToken(string username, string[] roles, int expiryMinutes = 60)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = _key.GetKey();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, string.Join(",", roles))
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(expiryMinutes),
+                Issuer = _issuer,
+                Audience = _audience,
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
+
