bf84d1812dab3c6050fe9d2384d5f7fbe82f5171
Михајловић Бојана <83251325+wildbohana@users.noreply.github.com>
2024-05-20 18:57:38 +0200

    restoring vote

---
+++ RedditClone/RedditService_WebRole/Controllers/VoteController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TableRepository;
+
+namespace RedditService_WebRole.Controllers
+{
+	[RoutePrefix("vote")]
+	public class VoteController : ApiController
+	{
+		TableRepositoryVote repo = new TableRepositoryVote();
+	}
+}
