55dd6bc5b4f832d469c4d3b5e7649a1865f9a3af
oli167 <116897781+oli167@users.noreply.github.com>
2024-05-20 17:19:03 +0200

    preparing for vote controller

---
+++ RedditClone/RedditService_WebRole/Models/Vote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedditService_WebRole.Models
+{
+    public class Vote
+    {
+        public string VoteId { get; set; }
+        public string UserId { get; set; }
+        public string PostId { get; set; }
+        public bool IsUpvote { get; set; }
+        public DateTime VotedAt { get; set; }
+    }
+}
