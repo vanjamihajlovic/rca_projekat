e005adec4863c7e9f218fbbc0ba3ca8348e224d4
Vanja Mihajlović <73716807+vanjamihajlovic@users.noreply.github.com>
2024-05-21 16:29:13 +0200

    Prebacena logika iz NSProvider u WorkerRole

--- RedditClone/NotificationService_WorkerRole/WorkerRole.cs
+++ RedditClone/NotificationService_WorkerRole/WorkerRole.cs
@@ -1,10 +1,16 @@
 ﻿using Helpers;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage.Queue;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using ServiceData;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using TableRepository;
 
 /*
 Prilikom postavljanja novog komentara na temu - RedditService rola šalje poruku u red (notifications) gde ubacuje ID komentara
@@ -19,12 +25,13 @@ Primer sistema za slanje mejlova jesu Postmark ili SendGrid.
 
 //mejl cloudproj@gmail.com
 //sifra cloudproj123
+//za sendGrid nalog je drugo
 
 
 
 namespace NotificationService_WorkerRole
 {
-    //dvdvdv
+
     public class WorkerRole : RoleEntryPoint
     {
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
@@ -36,11 +43,97 @@ namespace NotificationService_WorkerRole
         // private NotificationService ns = new NotificationService();
 
         private readonly QueueHelper queueHelper;
-        CloudQueue queueSubs = QueueHelper.GetQueueReference("RedditSubscribeTabela");
 
-        
+        CloudQueue queueComments = QueueHelper.GetQueueReference("CommentNotificationsQueue");
+        CloudQueue queueAdmins = QueueHelper.GetQueueReference("AdminNotificationsQueue");
 
-        
+
+        public async Task PosaljiMejl(string korisnikEmail, string tekstKomentara, string autorKomentara, DateTime vreme, string naslovTeme)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("cloudprojekat@gmail.com", "CLOUD Projekat Mejl");
+            var subject = "Novi komentar za temu : " + naslovTeme;
+            var to = new EmailAddress(korisnikEmail, "Example User");
+            var plainTextContent = autorKomentara + " commented: " + tekstKomentara;
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            //Console.WriteLine(response.StatusCode);
+            //Console.WriteLine(response.Body.ReadAsStringAsync());
+        }
+
+        #region Komentari
+        private async Task ProveriQueueKomentari()
+        {
+            if (queueComments.ApproximateMessageCount > 0)
+            {
+                string idKomentara = queueComments.GetMessage().AsString;
+
+                TableRepositoryKomentar trk = new TableRepositoryKomentar();
+                Komentar k = trk.DobaviKomentar(idKomentara);
+
+                TableRepositoryTema trt = new TableRepositoryTema();
+                Tema t = trt.DobaviTemu(k.IdTeme.ToString());
+
+
+                TableRepositorySubscribe trs = new TableRepositorySubscribe();
+                List<Subscribe> pretplaceni = trs.DobaviSvePrijavljene(t.Id.ToString());
+
+
+                string tekstKomentara = k.Sadrzaj;
+                string autorKomentara = k.Autor;
+                string naslovTeme = t.Naslov;
+                int brojMejlova = t.PretplaceniKorisnici.Count;
+                DateTime vreme = DateTime.Now;
+
+
+                foreach (Subscribe i in pretplaceni)
+                {
+                    //TableRepositoryKorisnik trkor = new TableRepositoryKorisnik();
+                    //Korisnik kor = trkor.DobaviKorisnika(pretplaceni.ToString());
+                    await PosaljiMejl(i.UserId, tekstKomentara, autorKomentara, vreme, naslovTeme);
+                }
+
+                UpisiNotifikacijuUTabelu(k.Id, vreme, brojMejlova);
+            }
+        }
+
+        private void UpisiNotifikacijuUTabelu(int idKomentara, DateTime vreme, int brojPoslatihMejlova)
+        {
+            Notifikacija n = new Notifikacija(idKomentara, vreme, brojPoslatihMejlova);
+            TableRepositoryNotifikacije trn = new TableRepositoryNotifikacije();
+            trn.SacuvajNotifikaciju(n);
+        }
+        #endregion
+
+        #region Provere
+        private void ProveriQueueProvere()
+        {
+            if (queueComments.ApproximateMessageCount > 0)
+            {
+                string idIzvestaja = queueAdmins.GetMessage().AsString;
+
+                TableRepositoryIzvestaj tri = new TableRepositoryIzvestaj();
+                Izvestaj i = tri.DobaviIzvestaj(idIzvestaja);
+
+                string poruka = i.Sadrzaj;
+                DateTime vreme = i.Vreme;
+
+                // TODO dalju logiku
+            }
+        }
+
+        private void UpisiIzvwstajUTabelu(int idIzvestaja, DateTime vreme, string poruka)
+        {
+            Izvestaj i = new Izvestaj(idIzvestaja, vreme, poruka);
+            TableRepositoryIzvestaj tri = new TableRepositoryIzvestaj();
+            tri.SacuvajIzvestaj(i);
+        }
+
+
+        #endregion
+
 
         public override void Run()
         {
@@ -97,6 +190,7 @@ namespace NotificationService_WorkerRole
         {
             while (!cancellationToken.IsCancellationRequested)
             {
+                //ovo vrv moze i da se obrise
                 Trace.TraceInformation("Working");
                 await Task.Delay(10000);
             }
