b9ef2d030bbb4e6ffa0baa7e6c1d9f14301468db
arpi223 <barsiarpi223@gmail.com>
2024-06-03 13:39:45 +0200

    Refactoring PostController

--- RedditClone/Helpers/JWT/ReadJWTKey.cs
+++ RedditClone/Helpers/JWT/ReadJWTKey.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Helpers.JWT
 {
-    public class ReadJWTKey
+    public class JwtTokenReader
     {
-        public IEnumerable<System.Security.Claims.Claim> GetJWTClaimsFromKey(string token) { 
-        
-            var tokenHandler = new JwtSecurityTokenHandler();
+        public string ExtractTokenFromAuthorizationHeader(System.Net.Http.Headers.AuthenticationHeaderValue authorizationHeader)
+        {
+            if (authorizationHeader == null)
+            {
+                return null;
+            }
 
-            return tokenHandler.ReadJwtToken(token).Claims;
+            string authHeaderString = authorizationHeader.ToString(); 
 
+            if (string.IsNullOrWhiteSpace(authHeaderString))
+                return null;
+
+            string[] parts = authHeaderString.Split(' ');
+            if (parts.Length != 2 || parts[0] != "Bearer")
+                return null;
+
+            return parts[1];
         }
+
+        public IEnumerable<Claim> GetClaimsFromToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadJwtToken(token);
+            return jwtToken.Claims;
+        }
+
+        public string GetClaimValue(IEnumerable<Claim> claims, string claimType)
+        {
+            var claim = claims.FirstOrDefault(c => c.Type == claimType);
+            return claim?.Value;
+        }
+
     }
 }
--- RedditClone/RedditService_WebRole/Controllers/PostController.cs
+++ RedditClone/RedditService_WebRole/Controllers/PostController.cs
@@ -15,72 +15,50 @@ using TableRepository;
 
 namespace RedditService_WebRole.Controllers
 {
-    [RoutePrefix("post")]
-	[EnableCors(origins: "*", headers: "*", methods: "*")]
-	public class PostController : ApiController
+
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PostController : ApiController
     {
-        TableRepositoryTema repo = new TableRepositoryTema();
-        TableRepositoryKomentar repoKom = new TableRepositoryKomentar();
-        TableRepositoryKorisnik repoKor = new TableRepositoryKorisnik();
+        private readonly TableRepositoryTema _repo;
+        private readonly TableRepositoryKorisnik _repoKorisnik;
+        private readonly TableRepositoryKomentar _repoKommentar;
+        private readonly JwtTokenReader _jwtTokenReader;
+
+        public PostController()
+        {
+            _repo = new TableRepositoryTema();
+            _repoKorisnik = new TableRepositoryKorisnik();
+            _repoKommentar = new TableRepositoryKomentar();
+            _jwtTokenReader = new JwtTokenReader();
+        }
 
         [HttpPost]
         [Route("create")]
-        public async Task<IHttpActionResult> PostPost(Post post)
+        public async Task<IHttpActionResult> CreatePost(Post post)
         {
             try
             {
-                if (ModelState.IsValid == false || post == null)
-                {
+                if (!ModelState.IsValid || post == null)
                     return BadRequest();
-                }
 
-
-                var authHeader = Request.Headers.Authorization;
-
-                if (authHeader == null || authHeader.Scheme != "Bearer" )
-                {
+                var token = _jwtTokenReader.ExtractTokenFromAuthorizationHeader(Request.Headers.Authorization);
+                if (token == null)
                     return Unauthorized();
-                }
 
-                var token = authHeader.Parameter;
+                var claims = _jwtTokenReader.GetClaimsFromToken(token);
 
-                var jwtReader = new ReadJWTKey();
+                var emailClaim = _jwtTokenReader.GetClaimValue(claims, "email");
+                var firstName = _jwtTokenReader.GetClaimValue(claims, "firstName");
+                var lastName = _jwtTokenReader.GetClaimValue(claims, "lastName");
 
-                var claims = jwtReader.GetJWTClaimsFromKey(token);
+                var newPost = new Tema(post.Id, post.Title, post.Content, emailClaim, firstName, lastName);
+                bool isAdded = await Task.FromResult(_repo.DodajTemu(newPost));
 
-
-                var emailClaim = claims.First(c => c.Type == "email").Value;
-                var firstName = claims.First(c => c.Type == "firstName").Value;
-                var lastName = claims.First(c => c.Type == "lastName").Value;
-
-                /*var userIdClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
-                if (userIdClaim == null)
-                {
-                    return Unauthorized();
-                }
-
-                string userId = userIdClaim.Value;
-
-                // Preuzimanje korisnika iz repozitorijuma
-                Korisnik korisnik = repoKor.DobaviKorisnika(userId);
-                if (korisnik == null)
-                {
-                    return BadRequest("Korisnik ne postoji.");
-                }
-                */
-                // Try to find the user ID claim from different claim types
-
-
-                // Korisnik korisnik = repoKor.DobaviKorisnika(post.UserId);
-                var noviPost = new Tema(post.Id, post.Title, post.Content, emailClaim, firstName, lastName);
-                bool isAdded = await Task.FromResult(repo.DodajTemu(noviPost));
                 if (!isAdded)
-                {
                     return BadRequest();
-                }              
 
-                return Ok("Post posted and notifications sent successfully.");
-
+                return Ok("Post created successfully.");
             }
             catch (Exception ex)
             {
@@ -88,6 +66,7 @@ namespace RedditService_WebRole.Controllers
             }
         }
 
+        
         [HttpPost]
         [Route("delete/{id}")]
         public async Task<IHttpActionResult> DeletePost(string id)
@@ -100,35 +79,31 @@ namespace RedditService_WebRole.Controllers
                     return BadRequest("Invalid post ID.");
                 }
 
-                // Pronađi temu po Id-u
-                Tema temaToDelete = await Task.FromResult(repo.DobaviTemu(id));
+                Tema temaToDelete = await Task.FromResult(_repo.DobaviTemu(id));
                 if (temaToDelete == null)
                 {                 
                     return BadRequest("Failed to delete post.");
                 }
-                List<Komentar> svi = repoKom.DobaviSve().ToList();
+                List<Komentar> svi = _repoKommentar.DobaviSve().ToList();
                 var p = svi.Where(x => x.IdTeme == temaToDelete.Id).ToList();
 
                 temaToDelete.Komentari = p.Select(x => x.RowKey).ToList();
 
 
-                // Briši komentare povezane sa temom
                 if (temaToDelete.Komentari != null)
                 {
-                    // Briši komentare povezane sa temom
                     foreach (string komentarId in temaToDelete.Komentari)
                     {
-                        bool isCommentDeleted = await Task.FromResult(repoKom.ObrisiKomentar(komentarId));
+                        bool isCommentDeleted = await Task.FromResult(_repoKommentar.ObrisiKomentar(komentarId));
                         if (!isCommentDeleted)
                         {
-                            // Ukoliko nije uspeo da obrise komentar, vrati false
                             return BadRequest("Failed to delete comment.");
                         }
                     }
                 }
 
                 // Delete post using repository
-                bool isDeleted = await Task.FromResult(repo.ObrisiTemu(id));
+                bool isDeleted = await Task.FromResult(_repo.ObrisiTemu(id));
                 if (!isDeleted)
                 {
                     return BadRequest("Failed to delete post.");
@@ -156,7 +131,7 @@ namespace RedditService_WebRole.Controllers
                 }
 
                 // Pretraživanje tema po naslovu
-                var results = await Task.FromResult(repo.PretraziTeme(searchTerm));
+                var results = await Task.FromResult(_repo.PretraziTeme(searchTerm));
 
                 // Pretvorba rezultata u listu i vraćanje HTTP odgovora
                 var searchResults = results.ToList();
@@ -175,7 +150,7 @@ namespace RedditService_WebRole.Controllers
             try
             {
                 // Preuzmi sve teme iz repozitorijuma
-                var allPosts = repo.DobaviSve();
+                var allPosts = _repo.DobaviSve();
 
                 // Sortiranje rezultata
                 switch (sortBy.ToLower())
@@ -209,7 +184,7 @@ namespace RedditService_WebRole.Controllers
                     return BadRequest("Invalid post ID.");
                 }
 
-                Tema post = await Task.FromResult(repo.DobaviTemu(id));
+                Tema post = await Task.FromResult(_repo.DobaviTemu(id));
                 if (post == null)
                 {
                     return NotFound();
@@ -229,7 +204,7 @@ namespace RedditService_WebRole.Controllers
         {
             try
             {
-                var allPosts = await Task.FromResult(repo.DobaviSve().ToList());
+                var allPosts = await Task.FromResult(_repo.DobaviSve().ToList());
                 return Ok(allPosts);
             }
             catch (Exception ex)
