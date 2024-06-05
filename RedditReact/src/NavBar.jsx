e10b017561b2982e119fabd5cac9455ce2d5210f
arpi223 <barsiarpi223@gmail.com>
2024-06-05 20:00:41 +0200

    Adding logout button

--- RedditReact/src/NavBar.jsx
+++ RedditReact/src/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './NavBar.css'; // Import your CSS file for styling
+import './NavBar.css';
+import Cookies from 'js-cookie';
+
+
+const handleLogout = () => {
+    Cookies.remove('jwt-token');
+};
 
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <NavLink to="/" className="nav-link" activeClassName="active-link">Home</NavLink>
-            <NavLink to="/create/topic" className="nav-link" activeClassName="active-link">Create Topic</NavLink>
-            <NavLink to="/profile" className="nav-link" activeClassName="active-link">Profile</NavLink>
+            <NavLink to="/" className="nav-link">Home</NavLink>
+            <NavLink to="/create/topic" className="nav-link">Create Topic</NavLink>
+            <NavLink to="/profile" className="nav-link">Profile</NavLink>
+            {Cookies.get('jwt-token') && <NavLink to="/login" className="nav-link logout-button" onClick={handleLogout}> Log Out </NavLink>}
         </nav>
     );
 };
