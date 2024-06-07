4cbafdaa867f65b6193a1a9f5fdd9b65b2688b33
oli167 <116897781+oli167@users.noreply.github.com>
2024-06-07 02:28:31 +0200

    hash pass for profileChange

--- RedditClone/RedditService_WebRole/Controllers/UserController.cs
+++ RedditClone/RedditService_WebRole/Controllers/UserController.cs
@@ -12,6 +12,8 @@ using TableRepository;
 using System.Drawing;
 using System.IO;
 using Helpers;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RedditService_WebRole.Controllers
 {
@@ -57,7 +59,11 @@ namespace RedditService_WebRole.Controllers
                     user.Grad = updatedUser.Grad ?? user.Grad;
                     user.Drzava = updatedUser.Drzava?? user.Drzava;
                     user.BrTel = updatedUser.BrTel ?? user.BrTel;
-                    user.Lozinka = updatedUser.Lozinka ?? user.Lozinka;
+
+                    if (!string.IsNullOrEmpty(updatedUser.Lozinka))
+                    {
+                        user.Lozinka = HashPassword(updatedUser.Lozinka);
+                    }
 
                     if (!updatedUser.Slika.StartsWith("http")) {
                         Image image;
@@ -118,6 +124,33 @@ namespace RedditService_WebRole.Controllers
             }
         }
 
+        
+
+        #region  Hash Helpers
+        static string HashPassword(string password)
+        {
+            byte[] salt = Encoding.UTF8.GetBytes("CGYzqeN4plZekNC88Umm1Q==");
+            using (var sha256 = new SHA256Managed())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                byte[] saltedPassword = new byte[passwordBytes.Length + salt.Length];
+
+                // Concatenate password and salt
+                Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+                Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+                // Hash the concatenated password and salt
+                byte[] hashedBytes = sha256.ComputeHash(saltedPassword);
+
+                // Concatenate the salt and hashed password for storage
+                byte[] hashedPasswordWithSalt = new byte[hashedBytes.Length + salt.Length];
+                Buffer.BlockCopy(salt, 0, hashedPasswordWithSalt, 0, salt.Length);
+                Buffer.BlockCopy(hashedBytes, 0, hashedPasswordWithSalt, salt.Length, hashedBytes.Length);
+
+                return Convert.ToBase64String(hashedPasswordWithSalt);
+            }
+        }
+        #endregion
     }
 
 }
